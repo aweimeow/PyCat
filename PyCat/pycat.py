d07fa450d7ac189b75abaf998cc11312118c6c3b
aweimeow <ccps960718456@gmail.com>
2017-03-30 19:01:16 +0800

    new structure

---
+++ PyCat/pycat.py
@@ -0,0 +1,18 @@
+def command(socket, cmd):
+    "Exec Command"
+    pass
+
+
+def portCheck(port):
+    "Boolean: check if a port is valid"
+    pass
+
+
+def ipCheck(ip):
+    "Boolean: check if an ip address is valid"
+    pass
+
+
+if __name__ == '__main__':
+    "Using argparse to get ip, port from input"
+    pass
---
+++ PyCat/scanner.py
@@ -0,0 +1,9 @@
+class Scanner:
+    def __init__(self):
+        pass
+
+    def scanip(self, ip):
+        pass
+
+    def scanport(self, ip, port):
+        pass
