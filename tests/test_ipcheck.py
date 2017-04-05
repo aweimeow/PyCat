26a5736b1274b1caa2641cc0a833b37b79502906
hsiaohsuan <hsiaohsuan1l1l@gmail.com>
2017-04-05 22:41:50 +0800

    Add netmask tests

--- tests/test_ipcheck.py
+++ tests/test_ipcheck.py
@@ -200,7 +200,9 @@ class TestBoundaryValue:
 class TestEquivalenceClass:
     def test_WeakNormal(self):
         assert pycat.ipCheck("128.128.128.128")
-        assert pycat.ipCheck("128.128 .128.128")
+        assert pycat.ipCheck("128.128.128.128/16")
+        assert pycat.ipCheck("128.128.128.128/255.255.0.0")
+        assert pycat.ipCheck("128.128.128.128/0.0.255.255")
 
     def test_WeakRobust(self):
         assert not pycat.ipCheck("128.-128.128.128")
@@ -208,6 +210,10 @@ class TestEquivalenceClass:
         assert not pycat.ipCheck("128.abc.128.128")
         assert not pycat.ipCheck("128.128,128.128")
         assert not pycat.ipCheck("128..128.128")
+        assert not pycat.ipCheck("128.128.128.128/-16")
+        assert not pycat.ipCheck("128.128.128.128/40")
+        assert not pycat.ipCheck("128.128.128.128/200.200.0.0")
+        assert not pycat.ipCheck("128.128.128.128/#$%")
 
     def test_StrongNormal(self):
         "Same as weak normal test"
@@ -216,20 +222,97 @@ class TestEquivalenceClass:
     def test_StrongRobust(self):
         assert not pycat.ipCheck("128.-128,128.128")
         assert not pycat.ipCheck("128.-128..128")
+        assert not pycat.ipCheck("128.-128.128.128/-16")
+        assert not pycat.ipCheck("128.-128.128.128/40")
+        assert not pycat.ipCheck("128.-128.128.128/200.200.0.0")
+        assert not pycat.ipCheck("128.-128.128.128/#$%")
+
         assert not pycat.ipCheck("128.400,128.128")
         assert not pycat.ipCheck("128.400..128")
+        assert not pycat.ipCheck("128.400.128.128/-16")
+        assert not pycat.ipCheck("128.400.128.128/40")
+        assert not pycat.ipCheck("128.400.128.128/200.200.0.0")
+        assert not pycat.ipCheck("128.400.128.128/#$%")
+
         assert not pycat.ipCheck("128.abc,128.128")
         assert not pycat.ipCheck("128.abc..128")
+        assert not pycat.ipCheck("128.abc.128.128/-16")
+        assert not pycat.ipCheck("128.abc.128.128/40")
+        assert not pycat.ipCheck("128.abc.128.128/200.200.0.0")
+        assert not pycat.ipCheck("128.abc.128.128/#$%")
+
         assert not pycat.ipCheck("128.,128.128")
+        assert not pycat.ipCheck("128.128,128.128/-16")
+        assert not pycat.ipCheck("128.128,128.128/40")
+        assert not pycat.ipCheck("128.128,128.128/200.200.0.0")
+        assert not pycat.ipCheck("128.128,128.128/#$%")
+
+        assert not pycat.ipCheck("128..128.128/-16")
+        assert not pycat.ipCheck("128..128.128/40")
+        assert not pycat.ipCheck("128..128.128/200.200.0.0")
+        assert not pycat.ipCheck("128..128.128/#$%")
+
         assert not pycat.ipCheck("128.-128,.128")
         assert not pycat.ipCheck("128.400,.128")
         assert not pycat.ipCheck("128.abc,.128")
+
+        assert not pycat.ipCheck("128.-128,128.128/-16")
+        assert not pycat.ipCheck("128.-128,128.128/40")
+        assert not pycat.ipCheck("128.-128,128.128/200.200.0.0")
+        assert not pycat.ipCheck("128.-128,128.128/#$%")
+
+        assert not pycat.ipCheck("128.400,128.128/-16")
+        assert not pycat.ipCheck("128.400,128.128/40")
+        assert not pycat.ipCheck("128.400,128.128/200.200.0.0")
+        assert not pycat.ipCheck("128.400,128.128/#$%")
+
+        assert not pycat.ipCheck("128.abc,128.128/-16")
+        assert not pycat.ipCheck("128.abc,128.128/40")
+        assert not pycat.ipCheck("128.abc,128.128/200.200.0.0")
+        assert not pycat.ipCheck("128.abc,128.128/#$%")
+
+        assert not pycat.ipCheck("128.-128..128/-16")
+        assert not pycat.ipCheck("128.-128..128/40")
+        assert not pycat.ipCheck("128.-128..128/200.200.0.0")
+        assert not pycat.ipCheck("128.-128..128/#$%")
+
+        assert not pycat.ipCheck("128.400..128/-16")
+        assert not pycat.ipCheck("128.400..128/40")
+        assert not pycat.ipCheck("128.400..128/200.200.0.0")
+        assert not pycat.ipCheck("128.400..128/#$%")
+
+        assert not pycat.ipCheck("128.abc..128/-16")
+        assert not pycat.ipCheck("128.abc..128/40")
+        assert not pycat.ipCheck("128.abc..128/200.200.0.0")
+        assert not pycat.ipCheck("128.abc..128/#$%")
+
+        assert not pycat.ipCheck("128.,128.128/-16")
+        assert not pycat.ipCheck("128.,128.128/40")
+        assert not pycat.ipCheck("128.,128.128/200.200.0.0")
+        assert not pycat.ipCheck("128.,128.128/#$%")
+
+        assert not pycat.ipCheck("128.-128,.128/-16")
+        assert not pycat.ipCheck("128.-128,.128/40")
+        assert not pycat.ipCheck("128.-128,.128/200.200.0.0")
+        assert not pycat.ipCheck("128.-128,.128/#$%")
+
+        assert not pycat.ipCheck("128.400,.128/-16")
+        assert not pycat.ipCheck("128.400,.128/40")
+        assert not pycat.ipCheck("128.400,.128/200.200.0.0")
+        assert not pycat.ipCheck("128.400,.128/#$%")
+
+        assert not pycat.ipCheck("128.abc,.128/-16")
+        assert not pycat.ipCheck("128.abc,.128/40")
+        assert not pycat.ipCheck("128.abc,.128/200.200.0.0")
+        assert not pycat.ipCheck("128.abc,.128/#$%")
 
 
 class TestEdge:
     def test_WeakNormal(self):
         assert pycat.ipCheck("0.0.0.0")
-        assert pycat.ipCheck("0 .0 .0 .0 ")
+        assert pycat.ipCheck("0.0.0.0/0")
+        assert pycat.ipCheck("0.0.0.0/0.0.0.0")
+        assert pycat.ipCheck("0.0.0.0/255.255.255.255")
 
     def test_WeakRobust(self):
         assert not pycat.ipCheck("-1.-1.-1.-1")
@@ -237,6 +320,10 @@ class TestEdge:
         assert not pycat.ipCheck("a.a.a.a")
         assert not pycat.ipCheck("0,0,0,0")
         assert not pycat.ipCheck("0...")
+        assert not pycat.ipCheck("0.0.0.0/-1")
+        assert not pycat.ipCheck("0.0.0.0/33")
+        assert not pycat.ipCheck("0.0.0.0/-1.-1.-1.-1")
+        assert not pycat.ipCheck("0.0.0.0/@")
 
     def test_StrongNormal(self):
         "Same as weak normal test"
@@ -245,14 +332,89 @@ class TestEdge:
     def test_StrongRobust(self):
         assert not pycat.ipCheck("-1,-1,-1,-1")
         assert not pycat.ipCheck("-1...")
+        assert not pycat.ipCheck("-1.-1.-1.-1/-1")
+        assert not pycat.ipCheck("-1.-1.-1.-1/33")
+        assert not pycat.ipCheck("-1.-1.-1.-1/-1.-1.-1.-1")
+        assert not pycat.ipCheck("-1.-1.-1.-1/@")
+
         assert not pycat.ipCheck("256,256,256,256")
         assert not pycat.ipCheck("256...")
+        assert not pycat.ipCheck("256.256.256.256/-1")
+        assert not pycat.ipCheck("256.256.256.256/33")
+        assert not pycat.ipCheck("256.256.256.256/-1.-1.-1.-1")
+        assert not pycat.ipCheck("256.256.256.256/@")
+
         assert not pycat.ipCheck("a,a,a,a")
         assert not pycat.ipCheck("a...")
+        assert not pycat.ipCheck("a.a.a.a/-1")
+        assert not pycat.ipCheck("a.a.a.a/33")
+        assert not pycat.ipCheck("a.a.a.a/-1.-1.-1.-1")
+        assert not pycat.ipCheck("a.a.a.a/@")
+
         assert not pycat.ipCheck("0,,,")
+        assert not pycat.ipCheck("0,0,0,0/-1")
+        assert not pycat.ipCheck("0,0,0,0/33")
+        assert not pycat.ipCheck("0,0,0,0/-1.-1.-1.-1")
+        assert not pycat.ipCheck("0,0,0,0/@")
+
+        assert not pycat.ipCheck("0.../-1")
+        assert not pycat.ipCheck("0.../33")
+        assert not pycat.ipCheck("0.../-1.-1.-1.-1")
+        assert not pycat.ipCheck("0.../@")
+
         assert not pycat.ipCheck("-1,,,")
         assert not pycat.ipCheck("256,,,")
         assert not pycat.ipCheck("a,,,")
+
+        assert not pycat.ipCheck("-1,-1,-1,-1/-1")
+        assert not pycat.ipCheck("-1,-1,-1,-1/33")
+        assert not pycat.ipCheck("-1,-1,-1,-1/-1.-1.-1.-1")
+        assert not pycat.ipCheck("-1,-1,-1,-1/@")
+
+        assert not pycat.ipCheck("256,256,256,256/-1")
+        assert not pycat.ipCheck("256,256,256,256/33")
+        assert not pycat.ipCheck("256,256,256,256/-1.-1.-1.-1")
+        assert not pycat.ipCheck("256,256,256,256/@")
+
+        assert not pycat.ipCheck("a,a,a,a/-1")
+        assert not pycat.ipCheck("a,a,a,a/33")
+        assert not pycat.ipCheck("a,a,a,a/-1.-1.-1.-1")
+        assert not pycat.ipCheck("a,a,a,a/@")
+
+        assert not pycat.ipCheck("-1.../-1")
+        assert not pycat.ipCheck("-1.../33")
+        assert not pycat.ipCheck("-1.../-1.-1.-1.-1")
+        assert not pycat.ipCheck("-1.../@")
+
+        assert not pycat.ipCheck("256.../-1")
+        assert not pycat.ipCheck("256.../33")
+        assert not pycat.ipCheck("256.../-1.-1.-1.-1")
+        assert not pycat.ipCheck("256.../@")
+
+        assert not pycat.ipCheck("a.../-1")
+        assert not pycat.ipCheck("a.../33")
+        assert not pycat.ipCheck("a.../-1.-1.-1.-1")
+        assert not pycat.ipCheck("a.../@")
+
+        assert not pycat.ipCheck("0,,,/-1")
+        assert not pycat.ipCheck("0,,,/33")
+        assert not pycat.ipCheck("0,,,/-1.-1.-1.-1")
+        assert not pycat.ipCheck("0,,,/@")
+
+        assert not pycat.ipCheck("-1,,,/-1")
+        assert not pycat.ipCheck("-1,,,/33")
+        assert not pycat.ipCheck("-1,,,/-1.-1.-1.-1")
+        assert not pycat.ipCheck("-1,,,/@")
+
+        assert not pycat.ipCheck("256,,,/-1")
+        assert not pycat.ipCheck("256,,,/33")
+        assert not pycat.ipCheck("256,,,/-1.-1.-1.-1")
+        assert not pycat.ipCheck("256,,,/@")
+
+        assert not pycat.ipCheck("a,,,/-1")
+        assert not pycat.ipCheck("a,,,/33")
+        assert not pycat.ipCheck("a,,,/-1.-1.-1.-1")
+        assert not pycat.ipCheck("a,,,/@")
 
 
 class TestDecisionTableBased:
