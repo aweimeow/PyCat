b2fe0a7107f514ee003be625b6d4d97b4bf50525
hsiaohsuan <hsiaohsuan1l1l@gmail.com>
2017-04-06 16:27:45 +0800

    Init boundary test for portCheck function

---
+++ tests/test_portcheck.py
@@ -0,0 +1,15 @@
+from PyCat import pycat
+
+
+class TestBoundaryValue:
+    def test_WeakNormal(self):
+        pass
+
+    def test_WeakRobust(self):
+        pass
+
+    def test_StrongNormal(self):
+        pass
+
+    def test_StrongRobust(self):
+        pass
