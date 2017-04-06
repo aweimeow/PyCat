19a2988fe17f8e5ecd72f5a780bb79f320f54440
hsiaohsuan <hsiaohsuan1l1l@gmail.com>
2017-04-06 16:52:47 +0800

    Failed boundary test for portCheck function

--- tests/test_portcheck.py
+++ tests/test_portcheck.py
@@ -3,13 +3,23 @@ from PyCat import pycat
 
 class TestBoundaryValue:
     def test_WeakNormal(self):
-        pass
+        assert pycat.portCheck(0)
+        assert pycat.portCheck("0")
+
+        assert pycat.portCheck(65535)
+        assert pycat.portCheck("65535")
 
     def test_WeakRobust(self):
-        pass
+        assert not pycat.portCheck(-1)
+        assert not pycat.portCheck("-1")
+
+        assert not pycat.portCheck(65536)
+        assert not pycat.portCheck("65536")
 
     def test_StrongNormal(self):
+        "Same as weak normal test"
         pass
 
     def test_StrongRobust(self):
+        "Same as weak robust test"
         pass
