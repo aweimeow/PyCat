79b4abb134c156acb6333e167fd30bd6f3f1cf6d
aweimeow <ccps960718456@gmail.com>
2017-04-08 00:09:10 +0800

    Complete basic PyCat scanner class

--- PyCat/scanner.py
+++ PyCat/scanner.py
@@ -1,9 +1,38 @@
+import socket
+
+
 class Scanner:
     def __init__(self):
-        pass
+        self.timeout = 1
 
     def scanip(self, ip):
-        pass
+        try:
+            socket.gethostbyaddr(ip)
+        except socket.herror:
+            return False
+        return True
 
     def scanport(self, ip, port):
-        pass
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.1)
+        return sock.connect_ex((ip, port))
+
+    def scanports(self, ip="127.0.0.1", ports="0-65535"):
+        assert ports.count('-') == 1
+        src_port, dst_port = map(lambda x: int(x), ports.split('-'))
+
+        if not self.scanip(ip):
+            self.report = {"success": False, "ports": {}}
+
+        self.report = {"success": True, "ports": {}}
+
+        for port in range(src_port, dst_port):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(0.1)
+            if sock.connect_ex((ip, port)) == 0:
+                self.report["ports"][port] = True
+            else:
+                self.report["ports"][port] = False
+
+            sock.close()
+
