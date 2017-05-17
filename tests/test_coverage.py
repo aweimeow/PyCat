3fb22c40894faa6e49547656a67bd46a9a6c4059
Hsiaohsuan <hsiaohsuan1l1l@gmail.com>
2017-05-17 21:36:43 +0800

    Finish coverage c1

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -22,3 +22,35 @@ class TestCoverage:
 
         assert scanner.scanip("127.0.0.1")
         assert not scanner.scanip("256.256.256.256")
+
+    def test_pycat_c1(self):
+        "Paths for portCheck"
+        assert pycat.portCheck(32768)
+        assert not pycat.portCheck(-32768)
+        assert not pycat.portCheck("abcde")
+
+        "Paths for ipCheck"
+        assert not pycat.ipCheck("128.128.128.-128/16")
+        assert not pycat.ipCheck("128.128.128.128/255.255.254.255")
+        assert not pycat.ipCheck("128.128.128.128/255.255.256.255")
+        assert not pycat.ipCheck("128.128.128.128/-16")
+        assert not pycat.ipCheck("128.128.128.-128")
+        assert not pycat.ipCheck("128.128")
+        assert not pycat.ipCheck("128.128.128.128/255.255.255.abc")
+        assert pycat.ipCheck("140.113.1.1/255.255.0.0")
+
+        "Paths for main"
+        assert not json.loads(pycat.main("140.113.1.1/-16", "32768"))["success"]
+        assert json.loads(pycat.main("140.113.1.1"))["success"]
+        assert not json.loads(pycat.main("140.113.1.1", "1-"))["success"]
+        assert not json.loads(pycat.main("140.113.1.1", "65536-32768"))["success"]
+        assert not json.loads(pycat.main("140.113.1.1", "32768-65536"))["success"]
+        assert not json.loads(pycat.main("140.113.1.1", "0,32768,65536"))["success"]
+        assert json.loads(pycat.main("140.113.1.1", "32768-65535"))["success"]
+        assert json.loads(pycat.main("140.113.1.1", "0,32768,65535"))["success"]
+        assert json.loads(pycat.main("140.113.1.1", "32768"))["success"]
+
+        "Paths for scanner"
+        scanner = Scanner()
+        assert not scanner.scanip("256.256.256.256")
+        assert scanner.scanip("127.0.0.1")
